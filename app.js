0f61705ba438d94d1ab453cc6d0b93a335443e0e
Mitch Gollub <mgollub@live.com>
2018-12-07 11:36:51 -0500

    using yargs

--- app.js
+++ app.js
@@ -2,20 +2,25 @@ console.log('Starting app...');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
+const argv = yargs.argv;
+
 var command = process.argv[2];
 console.log('Command: ', command);
+console.log('Process', process.argv);
+console.log('Yargs', argv);
 
 if (command === 'add') {
-    console.log('Adding new Note');
+    notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
-    console.log('Listing all Notes');
+    notes.getAll();
 } else if (command === 'read') {
-    console.log('Reading Note');
+    notes.getNote(argv.title);
 } else if (command === 'remove') {
-    console.log('Removing Note');
+    notes.removeNote(argv.title);
 } else {
     console.log('Command not recognized');
 }
--- notes.js
+++ notes.js
@@ -1,6 +1,24 @@
 console.log('Starting Notes...');
 
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'New Note'
+var addNote = (title, body) => {
+    console.log('Adding Note', title, body);
+}
+
+var getAll = () => {
+    console.log('Getting all notes');
+}
+
+var getNote = (title) => {
+    console.log('Getting Note', title);
+}
+
+var removeNote = (title) => {
+    console.log('Removing Note', title);
+}
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
 }
