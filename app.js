13a2be0f9629cab06d8ecf80578a668340c634c5
Mitch Gollub <mgollub@live.com>
2018-12-07 11:23:44 -0500

    finishing up user input section

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ if (command === 'add') {
     console.log('Adding new Note');
 } else if (command === 'list') {
     console.log('Listing all Notes');
+} else if (command === 'read') {
+    console.log('Reading Note');
+} else if (command === 'remove') {
+    console.log('Removing Note');
 } else {
     console.log('Command not recognized');
 }
