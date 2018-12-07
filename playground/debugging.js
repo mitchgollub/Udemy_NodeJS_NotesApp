4c9da6055261966555dd763d141dd5da8593cbb2
Mitch Gollub <mgollub@live.com>
2018-12-07 14:09:13 -0500

    debugging info

---
+++ playground/debugging.js
@@ -0,0 +1,15 @@
+var person = {
+    name: 'Mitch'
+};
+
+person.age = 27;
+
+debugger;
+
+console.table(person);
+
+/// Commands
+// node inspect <filename>
+// n = NEXT
+// c = CONTINUE
+// repl = Read-Eval-Print-Loop (REPLACE MODE)
